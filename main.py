5e43f725b022679141a4f0b13a16d9272df42fcf
pawan329 <pawan.sviet@gmail.com>
2024-07-04 19:00:16 +0530

    add you api key

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ OPENAIKEY = st.text_input(label="Enter your API Key he..")
 if OPENAIKEY:
     input_text = st.text_input(label="Please enter you text here..")
 
-llm = OpenAI(api_key = OPENAIKEY, temperature=0.6)
+    llm = OpenAI(api_key = OPENAIKEY, temperature=0.6)
 
-my_prompt = PromptTemplate(
-    input_variables=["content"],
-    template = "{content}"
-)
+    my_prompt = PromptTemplate(
+        input_variables=["content"],
+        template = "{content}"
+    )
 
-chain = LLMChain(llm = llm, prompt = my_prompt)
-response = chain.run(input_text)
+    chain = LLMChain(llm = llm, prompt = my_prompt)
+    response = chain.run(input_text)
 
 
-st.markdown(response)
+    st.markdown(response)
