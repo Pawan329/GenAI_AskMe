86a5fafaecfb9ab7115aa4c83c29bf57350d7eaf
pawan329 <pawan.sviet@gmail.com>
2024-07-04 18:34:30 +0530

    added dov env file

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import os
 
-OPEN_AI_API_KEY = "sk-proj-oylfNhdmm0Tfk3Rm8jBGT3BlbkFJPEqXFMEv5MOrVNzW98B0"
+# Load environment variables from the .env file (if present)
+load_dotenv()
+
+# Access environment variables as if they came from the actual environment
+SECRET_KEY = os.getenv('OPEN_AI_API_KEY')
+
 st.title("Ask me anything")
 
 input_text = st.text_input(label="Please enter you text here..")
 
-llm = OpenAI(api_key=OPEN_AI_API_KEY, temperature=0.6)
+llm = OpenAI(api_key = SECRET_KEY, temperature=0.6)
 
 my_prompt = PromptTemplate(
     input_variables=["content"],
