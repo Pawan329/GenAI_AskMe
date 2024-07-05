32f031a5f9491de2e0645c9fb4cc8253d2f5de12
pawan329 <pawan.sviet@gmail.com>
2024-07-06 00:03:29 +0530

    created .env file

--- main.py
+++ main.py
@@ -2,23 +2,31 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAIKEY = os.getenv('API_KEY')
 
 st.title("Ask me anything")
 
-OPENAIKEY = st.text_input(label="Enter your API Key he..")
-
-if OPENAIKEY == OPENAIKEY:
-    input_text = st.text_input(label="Please enter you text here..")
-
-    llm = OpenAI(api_key = OPENAIKEY, temperature=0.6)
-
-    my_prompt = PromptTemplate(
-        input_variables=["content"],
-        template = "{content}"
-    )
-
-    chain = LLMChain(llm = llm, prompt = my_prompt)
-    response = chain.run(input_text)
+input_text = st.text_input(label="Please enter you text here..")
+tone = st.sidebar.selectbox("Select Tone", ("Friend", "Assistant", "Expert"))
+word_limit = st.sidebar.slider("Select Word limit", min_value=10, max_value=50)
 
 
+llm = OpenAI(api_key = OPENAIKEY, temperature=0.6)
+
+my_prompt = PromptTemplate(
+    input_variables=["content","tone","word_limit"],
+    template = "Act as {tone}, tell me {content}. word limit is {word_limit}"
+)
+
+chain = LLMChain(llm = llm, prompt = my_prompt)
+response = chain.run({"content":input_text, "tone":tone, "word_limit":word_limit})
+
+if input_text == "":
+    response = ""
+else:
     st.markdown(response)
